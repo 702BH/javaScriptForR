712a7ebda43a9351ddfad78607874e44e38a80fb
702BH <65957905+702BH@users.noreply.github.com>
2023-10-14 19:01:18 +0300

    create boxxyoutput

--- Chapter 14/app.R
+++ Chapter 14/app.R
@@ -4,3 +4,32 @@ library(shiny)
 boxxy <- function(title, value, color = "#ef476f"){
   list(title = title, value = value, color = color)
 }
+
+boxxyOutput <- function(id){
+  # the HTML output
+  el <- tags$div(
+    id = id, class = "boxxy",
+    h1(
+      id = sprintf("%s-boxxy-value", id),
+      class = "boxxy-value"
+    ),
+    p(
+      id = sprintf("%s-boxxy-title", id),
+      class = "boxxy-title"
+    )
+  )
+  
+  path <- normalizePath("assets")
+  
+  deps <- list(
+    htmltools::htmlDependency(
+      name = "boxxy",
+      version = "1.0.0",
+      src = c(file = path),
+      script = c("countup.js", "binding.js"),
+      stylesheet = "style.css"
+    )
+  )
+  
+  htmltools::attachDependencies(el, deps)
+}
